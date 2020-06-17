d0963ef0871fa08c4bcaa1926c63886566ae177b
John Robling <roblingjohn@gmail.com>
2020-06-17 18:48:44 -0400

    fixed http/https issue on deployed site

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# gt-homework-06-weatherapp
+#Weather Dashboard
+
+## Description
+
+This page displays the current weather and forecast of an entered location, as well as clickable entries for previous searches.
+
+## Installation
+    
+1. pull repo to local machine
+2. open index.html
+
+## Usage
+
+[Deployed Site](https://roblingjohn.github.io/gt-homework-06-weatherapp/)
+![Home](./assets/home.png)
+
+For each hour block, the user can type in events to be saved to local memory. After entering the text, they can click the lock button in the right column to save the data.
+
+## Credits
+
+This page was modified by John Robling from the template given for the purposes of this class. CSS provided by Bootstrap (http://getbootstrap.com). Time elements provided by moment.js (https://momentjs.com/). 
--- script.js
+++ script.js
@@ -12,6 +12,7 @@
         // UV index
         var UVIndex = 0
 
+        // retrieves previous entries and enters empty array if not yet set
         var previousEntries = JSON.parse(localStorage.getItem("cities"));
         if (previousEntries === null) {
             previousEntries = []
@@ -28,22 +29,17 @@
         var weatherBox = $("#weather-box")
     // forecast box
 
-// function removeDuplicates(arrayName) {
-//     var uniqueArray = [];
-//     for(i=0; i < arrayName.length; i++){
-//         if(uniqueArray.indexOf(arrayName[i]) === -1) {
-//             uniqueArray.push(arrayName[i]);
-//         }
-//     }
-//     arrayName = uniqueArray;
-// }
-
+// write previous entries to left column of screen
 function writePrevEntries() {
+    // clear any entries already on screen
     $("#searched-cities").empty();
+    // iterate through array
     for (i = previousEntries.length - 1; i > previousEntries.length - 9 ; i--){
+        // stop loop if there are no values for this iteration
         if (previousEntries[i] === undefined) {
             console.log("No more entries");
         }
+        // add buttons with the names of previous entries
         else {
         $("#searched-cities").append("<button class='prev-search' data-city='" + previousEntries[i] + "'>" + previousEntries[i] + "</button>")
         }
@@ -55,6 +51,7 @@ tidyArray();
 writePrevEntries();
 getCityData();
 
+// limit array to 8 entries and prevent duplicates
 function tidyArray() {
     var uniqueArray = [];
         $.each(previousEntries, function(i, el){
@@ -66,6 +63,7 @@ function tidyArray() {
     }
 }
 
+// make UV index the relevant color of warning
 function UVColor() {
     if (UVIndex < 3) {
         $("#uv-index").attr("class", "uv-green");
@@ -78,14 +76,16 @@ function UVColor() {
     }
 }
 
-
+// set searchEntry as the value in the search bar
 searchEntry = $("#search-bar").val();
 
 // set event from search bar
 function getCityData() {
+    // reset search bar to empty
     $("#search-bar").val("")
     // get search results from search bar
     // get ajax data from search result
+    // set API key and query URL
     var APIKey = "83cea6cbcabbb7bef1222d329e94e37c"
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchEntry 
     + "&units=imperial&appid=" +
@@ -94,7 +94,9 @@ function getCityData() {
         url: queryURL,
         method: 'GET'
       }).then(function(response) {
+        //   unhide weather box
             weatherBox.attr("class", "")
+            // set variables for corresponding data points
             var results = response;
             cityName = results.name;
             temperature = Math.floor(results.main.temp);
@@ -103,7 +105,7 @@ function getCityData() {
             windspeed = windspeed.toFixed(1);
             longitude = results.coord.lon;
             lattitude = results.coord.lat;
-            // put results in appropriate fields in weather box
+            // display icon for current weather
             var currentWeather = response.weather[0].main;
             if (currentWeather === "Clear") {
                 var currentWeatherIcon = "fas fa-sun"
@@ -117,38 +119,49 @@ function getCityData() {
             else {
                 var currentWeatherIcon = currentWeather
             }
+            // put results in appropriate fields in weather box
             $("#city-name").text(cityName + "  (" + moment().format('L') + ")");
             $("#weather-icon").attr("class", currentWeatherIcon)
             $("#temperature").text("Temperature: " + temperature + "°F")
             $("#humidity").text("Humidity: " + humidity + "%")
             $("#wind-speed").text("WInd speed: " + windspeed + " MPH")
+            // put search entry into previous array and local storage
             previousEntries.push(cityName);
             localStorage.setItem("cities", JSON.stringify(previousEntries))
-            var UVIURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lattitude + "&lon=" + longitude;
+            // get UV index
+            var UVIURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lattitude + "&lon=" + longitude;
             $.ajax({
                 url: UVIURL,
                 method: 'GET'
             })
         .then(function(response) {
+            // set UV index text
             UVIndex = response.value;
             $("#uv-index").text(UVIndex);
+            // make UV index corresponding color
             UVColor();
             // create forecast information
-            var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + cityName + "&appid=" + APIKey;
+            var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + cityName + "&appid=" + APIKey;
             $.ajax({
                 url: forecastURL,
                 method: 'GET'
             })
         .then(function(response) {
             forecastAPI = response;
+            // unhide forecast block row
             $("#forecast-text").attr("class", "")
             var forecastBlocks = $("#forecast-blocks");
+            // clear any existing forecast blocks
             forecastBlocks.empty();
+        // iterate through daily forecast
         for (i = 1; i < 6; i++) {
+            // create forecast block
             var forecastEntry = $("<div class ='col-md forecast-box'>")
+            // set variables for forecast block information
             var forecastDate = moment(moment().add('days', i)).format("MM/DD/YYYY");
             forecastEntry.append("<h5>" + forecastDate + "</h5>");
             var forecastWeather = forecastAPI.list[i].weather[0].main;
+            // set forecast weather icons
             if (forecastWeather === "Clear") {
                 var forecastWeatherIcon = "<i class='fas fa-sun'></i>"
             }
@@ -161,16 +174,19 @@ function getCityData() {
             else {
                 var forecastWeatherIcon = forecastWeather
             }
+            // add information to weather block
             forecastEntry.append(forecastWeatherIcon);
             var forecastTemp = forecastAPI.list[i].main.temp;
             forecastTemp = Math.floor(forecastTemp);
             forecastEntry.append("<p>Temp: " + forecastTemp + "°F</p>");
             var forecastHumidity = forecastAPI.list[i].main.humidity;
             forecastEntry.append("<p>Humidity: " + forecastHumidity + "%</p>")
+            // add weather block to row
             forecastBlocks.append(forecastEntry)
 
 
         }
+        // allow previous searches to be clickable
         $(".prev-search").click(function(event) {
             event.preventDefault();
             searchEntry = $(this).attr("data-city");
@@ -179,15 +195,10 @@ function getCityData() {
     })
  })
 
-
-    // iterate through forecast 5 times
-
-// push search into previous search array
-// iterate through previous searchest and place in previous search column
-
 })
 }
 
+// sets click function for search bar
 $(".search-button").click(function() {
     event.preventDefault();
     searchEntry = $("#search-bar").val();
