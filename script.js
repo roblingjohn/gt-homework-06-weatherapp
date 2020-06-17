711f1f49975631f1d11cf4895376f1d0c2ec7a98
John Robling <roblingjohn@gmail.com>
2020-06-16 22:43:45 -0400

    added functionality to display previous searches

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Set variables 
     // search entry
-    var searchEntry = $("#search-bar").val();
     // ajax results
         // city name
         var cityName = ""
@@ -13,17 +12,48 @@
         // UV index
         var UVIndex = 0
 
-        var previousEntries = []
+        var previousEntries = JSON.parse(localStorage.getItem("cities"));
+        if (previousEntries === null) {
+            previousEntries = []
+        }
+        var searchEntry = previousEntries[previousEntries.length - 1];
+
         var forecastAPI = ""
 
     // weather box
+        var weatherBox = $("#weather-box")
     // forecast box
 
+// function removeDuplicates(arrayName) {
+//     var uniqueArray = [];
+//     for(i=0; i < arrayName.length; i++){
+//         if(uniqueArray.indexOf(arrayName[i]) === -1) {
+//             uniqueArray.push(arrayName[i]);
+//         }
+//     }
+//     arrayName = uniqueArray;
+// }
+
+function writePrevEntries() {
+    $("#searched-cities").empty();
+    for (i = previousEntries.length - 1; i > previousEntries.length - 9 ; i--){
+        if (previousEntries[i] === undefined) {
+            console.log("boop");
+        }
+        else {
+        $("#searched-cities").append("<p class='prev-search'>" + previousEntries[i] + "</p>")
+        }
+    }
+};
+
+writePrevEntries();
+getCityData();
+
+var searchEntry = $("#search-bar").val();
+
 // set event from search bar
-$("#search-button").on("click", function() {
-    event.preventDefault();
+function getCityData() {
     // get search results from search bar
-    var searchEntry = $("#search-bar").val();
     // get ajax data from search result
     var APIKey = "83cea6cbcabbb7bef1222d329e94e37c"
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchEntry 
@@ -33,6 +63,7 @@ $("#search-button").on("click", function() {
         url: queryURL,
         method: 'GET'
       }).then(function(response) {
+            weatherBox.attr("class", "")
             var results = response;
             cityName = results.name;
             temperature = Math.floor(results.main.temp);
@@ -48,8 +79,8 @@ $("#search-button").on("click", function() {
             $("#humidity").text("Humidity: " + humidity + "%")
             $("#wind-speed").text("WInd speed: " + windspeed + " MPH")
             previousEntries.push(cityName);
-            console.log(previousEntries)
-
+            localStorage.setItem("cities", JSON.stringify(previousEntries))
+            writePrevEntries();
             var UVIURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lattitude + "&lon=" + longitude;
             $.ajax({
                 url: UVIURL,
@@ -60,19 +91,17 @@ $("#search-button").on("click", function() {
             $("#uv-index").text("UV index: " + UVIndex);
             // create forecast information
             var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + cityName + "&appid=" + APIKey;
-            console.log(forecastURL);
             $.ajax({
                 url: forecastURL,
                 method: 'GET'
             })
         .then(function(response) {
             forecastAPI = response;
-            console.log(forecastAPI);
             $("#forecast-text").attr("class", "")
-
-        for (i = 1; i < 6; i++) {
             var forecastBlocks = $("#forecast-blocks");
-            var forecastEntry = $("<div class ='col forecast-box'>")
+            forecastBlocks.empty();
+        for (i = 1; i < 6; i++) {
+            var forecastEntry = $("<div class ='col-md forecast-box'>")
             var forecastDate = moment(moment().add('days', i)).format("MM/DD/YYYY");
             forecastEntry.append("<h5>" + forecastDate + "</h5>");
             var forecastWeatherIcon = forecastAPI.list[i].weather[0].main;
@@ -80,9 +109,9 @@ $("#search-button").on("click", function() {
             forecastEntry.append("<p>" + forecastWeatherIcon + "</p>");
             var forecastTemp = forecastAPI.list[i].main.temp;
             forecastTemp = Math.floor(forecastTemp);
-            forecastEntry.append("<p>Temperature: " + forecastTemp + "</p>");
+            forecastEntry.append("<p>Temp: " + forecastTemp + "°F</p>");
             var forecastHumidity = forecastAPI.list[i].main.humidity;
-            forecastEntry.append("<p>Humidity: " + forecastHumidity + "</p>")
+            forecastEntry.append("<p>Humidity: " + forecastHumidity + "%</p>")
             forecastBlocks.append(forecastEntry)
 
 
@@ -95,7 +124,18 @@ $("#search-button").on("click", function() {
 
 // push search into previous search array
 // iterate through previous searchest and place in previous search column
+
 })
+}
+
+$("#search-button").on("click", function() {
+    event.preventDefault();
+    searchEntry = $("#search-bar").val();
+    getCityData();
 })
 
-// http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
+$(".prev-search").on("click", function(){
+    event.preventDefault();
+    searchEntry = $(this).text();
+    getCityData();
+})
